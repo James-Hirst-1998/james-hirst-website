9bb881b9252eedd928296fb2aa74222912850c14
James Hirst <hirst.jj@googlemail.com>
2026-07-10 09:49:15 +0100

    Sea creature improvements'

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import DivePage from "./pages/Dive";
 
 const BowlOfFishPage = lazy(() => import("./pages/BowlOfFish"));
+const CreaturesPage = lazy(() => import("./pages/Creatures"));
 
 const App = () => (
   <Suspense fallback={null}>
     <Routes>
       <Route path="/" element={<DivePage />} />
       <Route path="/BowlOfFish" element={<BowlOfFishPage />} />
+      <Route path="/creatures" element={<CreaturesPage />} />
     </Routes>
   </Suspense>
 );
